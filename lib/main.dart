d3a4465ee342096f46c1f691416fd831e28172c1
kerolos129Yousef <kerolos.yousef129@gmail.com>
2022-12-06 17:39:07 +0200

    add task bottom sheet , Dialoge_utilities(show message,show loading, hide loading) , config firebase and firecloude , make a data class for task and config firebase to it with function withConvarter(), finish func/insertTask(),

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/Home/home_screen.dart';
+import 'package:todo/firebase_options.dart';
 import 'package:todo/my_theme.dart';
 
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
